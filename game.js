d39c87e8f2ff1920bfef54f713d094e71f3f6a0b
dpomykanov <dens.pm@gmail.com>
2019-10-13 15:05:07 +0200

    menu screen finished

--- game.js
+++ game.js
@@ -39,6 +39,10 @@ function preload()
 
 function create()
 {
+  game.scene.add('Menu', Menu);
+  game.scene.start('Menu');
+  this.spacePressed = false;
+
   const backgroundImage = this.add.image(-500,0,'background').setOrigin(0.0);
   backgroundImage.setScale(16, 3.5);
 
@@ -101,7 +105,8 @@ function create()
   this.a_key = this.input.keyboard.addKey('A');
   this.d_key = this.input.keyboard.addKey('D');
   this.w_key = this.input.keyboard.addKey('W');
-  
+  this.space_key = this.input.keyboard.addKey('SPACE');
+
   this.cameras.main.startFollow(this.player);
   playerReset(this.player);
 
@@ -135,6 +140,11 @@ function create()
 
 function update()
 {
+  if (this.space_key.isDown){
+    this.scene.remove('Menu');
+    this.spacePressed = true;
+  }
+
   reIgniteFire(this.fires);
   fireWaterCollision(this.fires.getChildren(), this.waters.getChildren());
 
@@ -146,32 +156,34 @@ function update()
 
 
   //----------Movement----------
+  if(this.spacePressed){
+    if(this.cursors.left.isDown || this.a_key.isDown)
+    {
+      this.player.setVelocityX(-300);
+      if(this.player.body.onFloor()) { this.player.play('walk', true); }
+    }
+    else if(this.cursors.right.isDown || this.d_key.isDown)
+    {
+      this.player.setVelocityX(300);
+      if(this.player.body.onFloor()) { this.player.play('walk', true); }
+    }
+    else
+    {
+      this.player.setVelocityX(0);
+      if(this.player.body.onFloor()) { this.player.play('idle', true); }
+    }
 
-  if(this.cursors.left.isDown || this.a_key.isDown)
-  {
-    this.player.setVelocityX(-300);
-    if(this.player.body.onFloor()) { this.player.play('walk', true); }
-  }
-  else if(this.cursors.right.isDown || this.d_key.isDown)
-  {
-    this.player.setVelocityX(300);
-    if(this.player.body.onFloor()) { this.player.play('walk', true); }
-  }
-  else
-  {
-    this.player.setVelocityX(0);
-    if(this.player.body.onFloor()) { this.player.play('idle', true); }
-  }
-
-  if((this.cursors.up.isDown || this.cursors.space.isDown || this.w_key.isDown) && this.player.body.onFloor())
-  {
-    this.player.setVelocityY(-350);
-    this.player.play('jump', true);
-    let jump = this.sound.add('jump');
-    jump.play();
+    if((this.cursors.up.isDown || this.cursors.space.isDown || this.w_key.isDown) && this.player.body.onFloor())
+    {
+      this.player.setVelocityY(-350);
+      this.player.play('jump', true);
+      let jump = this.sound.add('jump');
+      jump.play();
 
 
+    }
   }
+
 
   //----------Water----------
   pointer = this.input.activePointer;
